5fe60d8f005499804feafd25fa5bf5b6a11152c4
janlorenz <post@janlo.de>
2016-06-09 18:40:13 +0200

    Initial Commit

---
+++ make.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/janlo/Documents/literature
+pdftk A=Lorenz2009Universalityofmovie.pdf B=Metz.Lorenz2013Becomewhoyou.pdf C=Lorenz2015ModelingEvolutionIdeological.pdf D=Groeber.Lorenz.ea2014Dissonanceminimizationas.pdf E=Shin.Lorenz2010TippingDiffusivityin.pdf F=Koenig.Lorenz.ea2016Innovationvs.Imitation.pdf G=Lorenz.Paetzel.ea2013Redistributionspursgrowth.pdf H=Lorenz.Paetzel.ea2016JustDontCall.pdf I=Lorenz.Rauhut.ea2011HowSocialInfluence.pdf J=Rauhut.Lorenz.ea2011ReplytoFarrell.pdf  K=Lorenz.Rauhut.ea2015MajoritarianDemocracyUndermines.pdf L=Lorenz.Rauhut.ea2015MajoritarianDemocracyUnderminesAppendix.pdf M=Rauhut.Lorenz2010wisdomofcrowds.pdf cat A2-end B C D2-end E2-end F G H I J K L M2-end output all.pdf
+mv all.pdf /home/janlo/Documents/habil
